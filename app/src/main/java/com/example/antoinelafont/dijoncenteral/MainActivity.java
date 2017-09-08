e809ca187825aeabc4d0d07ee0d5885e66fb53eb
Antoine LAFONT <Antoine LAFONT>
2017-09-08 14:45:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/antoinelafont/dijoncenteral/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.antoinelafont.dijoncenteral;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
